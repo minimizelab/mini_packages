a9d5940587481870a33c67fad23661f4d14e6bb4
Axel Ekwall <axel@minimize.se>
2019-09-23 16:18:35 +0200

    formatting

--- packages/gatsby_example/gatsby-config.js
+++ packages/gatsby_example/gatsby-config.js
@@ -1,3 +1,3 @@
 module.exports = {
   plugins: ['mini_gatsby-theme-core'],
-};
+}
---
+++ packages/gatsby_example/src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from 'react'
+
+const Index: FunctionComponent = () => <div>Hello World</div>
+
+export default Index
